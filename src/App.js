e4da9db69a762a32f61b4688ef26dd4d18d07f0f
jdp111 <jdpearce111@gmail.com>
2023-04-14 16:23:04 -0600

    added api helper functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  useEffect(()=>{
+    async function effect() {
+    const localToken = localStorage.getItem('token') || null
+    const localUser = localStorage.getItem('username') || null
+    
+    if (localToken && localUser) {
+      const {username, firstName, lastName, email}  = await JoblyApi.getUser(localUser)
+      setCurrentUser({username, firstName, lastName, email} )
+    }
+  } effect()
+  },[])
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
         </a>
       </header>
     </div>
---
+++ src/api.js
@@ -0,0 +1,87 @@
+import axios from "axios";
+
+const BASE_API_URL = "http://localhost:3001";
+
+
+class HskApi {
+
+
+    // the token for interactive with the API will be stored here.\
+  
+  static async request(endpoint, data = {}, method = "get") {
+
+    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
+    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
+    const token = localStorage.getItem('token') || ""
+    const url = `${BASE_API_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${token}` };
+    const params = (method === "get")
+        ? data
+        : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  static async login(userInfo){
+    let res = await this.request(`auth/token`,userInfo, "get");
+    return res.token
+  }
+
+  static async register(userInfo){
+    let res = await this.request(`auth/register`,userInfo, "post");
+    return res.token
+  }
+
+  static async getCardsByUser(username){
+    let res = await this.request(`cards/${username}`)
+    return res
+  }
+
+  static async getCardsByUserGroup(username,groupId){
+    let res = await this.request(`cards/${username}/${groupId}`)
+    return res
+  }
+
+
+  static async updateGroup(username, cardId, groupNumber){
+    let endpoint = `cards/${username}`
+    let data = {"cardId": cardId, "groupNumber": groupNumber}
+    let res = await this.request(endpoint, data,"put")
+    return res
+  }
+
+  static async addCards(username, cards){
+    let endpoint = `cards/add/${username}`
+    let data = {"cards": cards}
+    let res = await this.request(endpoint, data,"post")
+    return res
+  }
+
+  static async deleteCards(username, cards){
+    let endpoint = `cards/delete/${username}`
+    let data = {"cards":cards}
+    let res = await this.request(endpoint, data,"delete")
+    return res
+  }
+
+  static async IncreaseSession(username){
+    let endpoint = `users/${username}`
+    let res = await this.request(endpoint,{},"put")
+    return res
+  }
+
+  static async getWords(searchBy = "simplified", searchTerm = ""){
+    let endpoint = `words?${searchBy}=${searchTerm}`
+    let res = await this.request(endpoint)
+    return res
+  }
+
+}
+
+export default JoblyApi;
